98652ac891321dd7b462aedab9660e3a3368de4e
Atharva-Devloper <atharvatambe00002@gmail.com>
2025-08-25 22:45:40 +0530

    Did Changes in index.js ,added Sample Data by data.json ,added homepage with home.ejs and did minor change in different file such as package.jason,header.ejs,subreddit.ejs .

--- projects/Tambe_Atharva/index.js
+++ projects/Tambe_Atharva/index.js
@@ -5,7 +5,10 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 import fs from 'fs';
-const redditData = JSON.parse(await fs.readFile('./data.json', 'utf-8'));
+const redditData = JSON.parse(
+    await fs.promises.readFile('./data.json', 'utf-8')
+);
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,6 +16,29 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
+
+// ...existing code...
+
+
+const subredditList = Object.keys(redditData);
+
+
+app.get('/', (req, res) => {
+    res.render('index', { subredditList });
+});
+
+// Update /r/:subreddit route
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit', { ...data, subredditList });
+    } else {
+        res.render('notfound', { subreddit, subredditList });
+    }
+});
+
+
 
 app.get('/r/:subreddit' ,(req ,res)=>{
     const {subreddit} = req.params;
